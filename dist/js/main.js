d9189ab31f09d8ab9f4aa6f649468c8d3d1560ed
Aaron P. Asuncion <aaronpasuncion@gmail.com>
2019-07-16 15:11:11 -0400

    main.js organization, responsiveness adjustments

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,66 +3,37 @@ $(document).ready(function() {
 
   // about section
   $("#aboutMe").on("click", function() {
-    var $barCapacity = $(".bar-capacity");
-    if ($("#rightBanner").hasClass("banner-link-click")) {
-      $("#rightBanner").removeClass("banner-link-click");
+    if ($("#rightBanner").hasClass("banner-active")) {
+      //  hide the creation content section
+      $("#rightBanner").removeClass("banner-active");
       $("#create").removeClass("creation-content");
-      $("#create").addClass("opacity-hide");
+      aboutDisplay();
+    } else if ($("#leftBanner").hasClass("banner-active")) {
+      aboutHide();
+      showLogo();
+    } else {
+      // creation content display to none
+      $(".creation-content").css("display", "none");
+      //display about content
+      aboutDisplay();
+      hideLogo();
     }
-
-    //add about-me class to implement animation
-    $(".about-me h3").css("display", "flex");
-    //retrieve the text of the percentrage of each skill and use that value for the width of the skill bar
-    $barCapacity.each(function(index) {
-      var $barFill = $(this)
-        .next()
-        .text();
-      $(this).css("width", $barFill);
-    });
-
-    // toggle center logo
-    $(".right-side").css({
-      animation: "scaleX-hide 1s forwards"
-    });
-    $(".left-side").css({
-      animation: "scaleX-hide 1s forwards"
-    });
-
-    // creation content display to none
-    $(".creation-content").css("display", "none");
-    // toggle the left banner
-    $("#leftBanner").toggleClass("banner-link-click");
-    // toggle opacity-hide to either hide or display the content of the about section
-    $(".about-content").toggleClass("opacity-hide");
-    // about-right / personal skills display
-    $(".about-right").css("display", "flex");
-    $(".p-skill").css("display", "flex");
   });
 
   // creation section
   $("#creation").on("click", function() {
-    if ($("#leftBanner").hasClass("banner-link-click")) {
-      $("#leftBanner").removeClass("banner-link-click");
-      $(".about-content").addClass("opacity-hide");
+    if ($("#leftBanner").hasClass("banner-active")) {
+      aboutHide();
+      creationDisplay();
+    } else if ($("#rightBanner").hasClass("banner-active")) {
+      creationHide();
+      showLogo();
+      // hide the personal skills section in the about content
+      $(".about-right").css("display", "none");
+    } else {
+      creationDisplay();
+      hideLogo();
     }
-    // if ($("#create").hasClass("creation-content")) {
-    //   $("#create").css("display", "none");
-    // }
-
-    // set about-right/personal skills display to none to allow more space for creationn section
-    $(".about-right").css("display", "none");
-
-    // toggle center logo
-    $(".right-side").css({
-      animation: "scaleX-hide 1s forwards"
-    });
-    $(".left-side").css({
-      animation: "scaleX-hide 1s forwards"
-    });
-
-    $("#create").toggleClass("opacity-hide");
-    $("#create").toggleClass("creation-content");
-    $("#rightBanner").toggleClass("banner-link-click");
   });
 
   // Side Menu
@@ -79,4 +50,76 @@ $(document).ready(function() {
     $("body").css("overflow", "auto");
     $("#lgMenu").css("width", "0px");
   });
+
+  // functions
+  /* ABOUT SECTION FUNCTIONS*/
+
+  //display about section
+  function aboutDisplay() {
+    let $barCapacity = $(".bar-capacity");
+    //retrieve the text of the percentrage of each skill and use that value for the width of the skill bar
+    $barCapacity.each(function(index) {
+      var $barFill = $(this)
+        .next()
+        .text();
+      $(this).css("width", $barFill);
+    });
+
+    //display personal skills section
+    $("#leftBanner").addClass("banner-active");
+    $(".about-content").removeClass("opacity-hide");
+    $(".about-right").css("display", "flex");
+    $(".p-skill").css("display", "flex");
+    $(".about-me h3").css("display", "flex");
+    if (!$("#create").hasClass("opacity-hide")) {
+      $("#create").addClass("opacity-hide");
+    }
+  }
+
+  // hide left banner content
+  function aboutHide() {
+    $("#leftBanner").removeClass("banner-active");
+    $(".about-content").addClass("opacity-hide");
+    $(".about-right").css("display", "none");
+    $(".p-skill").css("display", "none");
+  }
+
+  /* CREATION SECTION FUNCTIONS*/
+  // display creation section
+  function creationDisplay() {
+    // display right banner content and remove opacity hide
+    $("#rightBanner").addClass("banner-active");
+    $("#create").removeClass("opacity-hide");
+    $("#create").addClass("creation-content");
+  }
+
+  // hide creation section
+  function creationHide() {
+    // if user presses the creation link the second time, re-apply opacity hide styles and remove creation-content and banner-active classes
+    $("#rightBanner").removeClass("banner-active");
+    $("#create").removeClass("creation-content");
+    $("#create").addClass("opacity-hide");
+  }
+
+  // hide center logo function
+  function hideLogo() {
+    // toggle center logo
+    $(".right-side").css({
+      animation: "scaleX-hide 1s forwards"
+    });
+    $(".left-side").css({
+      animation: "scaleX-hide 1s forwards"
+    });
+  }
+
+  // display center logo function
+  function showLogo() {
+    // toggle center logo
+    $(".right-side").css({
+      animation: "scaleX-display 1s forwards "
+    });
+    $(".left-side").css({
+      animation: "scaleX-display 1s forwards "
+    });
+  }
 });
